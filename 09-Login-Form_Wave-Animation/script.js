1c2b5a1a6a6d2be9d867d74add104924c6a241ec
Richard Rosario <78895761+RichieDevR@users.noreply.github.com>
2022-01-27 01:30:54 -0500

    made transition delay timer longer

--- 09-Login-Form_Wave-Animation/script.js
+++ 09-Login-Form_Wave-Animation/script.js
@@ -4,7 +4,7 @@ const labels = document.querySelectorAll('.form-control label')
 labels.forEach(label => {
 	label.innerHTML = label.innerText
 		.split('')
-		.map((letter, idx) => `<span style = "transition-delay:${idx * 10}ms">${letter}</span>`)
+		.map((letter, idx) => `<span style = "transition-delay:${idx * 30}ms">${letter}</span>`)
 	  .join('')
 
 })
